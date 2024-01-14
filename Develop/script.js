43597e3bd75ff644781ccf5a8c9375ecb5c8f81e
Alexsander Rafael <alexsanderrafael2022@gmail.com>
2024-01-13 23:56:28 -0500

    started function to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,25 @@ var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
 var nums = "0123456789";
 var symbols = "!@#$%&*()?+-=_<>";
 
+
+  // function to generate password
+
+var generatePassword = function() {
+  var passwordLength = prompt("Choose a length of at least 8 characters and no more than 128 characters");
+    if (passwordLength < 8){
+      alert("The password has to be at least 8 characters");
+      return generatePassword();
+    } else if (passwordLength > 128){
+      alert("The password cannot be more than 128 characters");
+      return generatePassword();
+    }
+
+    //variable to append the options picked by the user
+    var characterSet = "";
+
+  }
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
